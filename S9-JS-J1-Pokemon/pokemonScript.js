a46f83c507c9cb0d6e6ecaab13ff86f7cdb41ad1
Ewan <ewanbaroneb.com@gmail.com>
2024-11-26 23:21:53 +0100

    Began work an pokedex api exercise

---
+++ S9-JS-J1-Pokemon/pokemonScript.js
@@ -0,0 +1,17 @@
+/* J'ai jamais joué a pokemon mais bon vas-y lets go c'est parti */
+
+//api/v1/pokemon/limit/100
+
+function DisplayHomePage() {
+    
+}
+
+function CreatePokemonCardElement() {
+
+}
+
+function RequestPokemonApi() {
+
+}
+
+DisplayHomePage();
